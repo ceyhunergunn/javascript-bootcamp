f850c714d3e3c155761141cf1fa82a0a0f133fd5
ceyhun <ceyhun@uzelbilisim.com>
2024-01-23 14:45:07 +0300

    started js course and started 01-Fundamentals-Part-1

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,95 @@
+/* let js = "amaizing";
+
+ if (js === "amaizing") {
+   alert("JS is FUN");
+ }
+ console.log(31 + 61 + 52);
+
+ ******** VALUES AND VARIABLES ******** 
+
+ console.log("Jonas");
+ console.log(23);
+ console.log(30 + 27 + 4);
+
+ let firstName = "Jonas";
+ let first = "Matilda";
+ let firstNamePerson = "Ruby";
+ let first_name_person = "Ceyhun";
+
+ console.log(firstName);
+ console.log(first);
+ console.log(firstNamePerson);
+ console.log(first_name_person);
+
+ let PI = 3.1415;
+
+ More understandable
+ let myFirstJob = "Programmer";
+ let myCurrentJob = "Teacher";
+
+ Less understandable
+
+ let job1 = "Programmer";
+ let job2 = "Teacher";
+
+******** DATA TYPES ******** 
+
+let age = 23; //number
+let firstName = "Jonas"; //string
+let javascriptIsFun = true; //boolen
+let year; //undefined
+
+console.log(typeof javascriptIsFun);
+console.log(typeof age);
+console.log(typeof firstName);
+console.log(typeof year);
+
+// Changed type
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+//Changed type
+year = 2024;
+console.log(typeof year);
+
+
+******** LET, CONST AND VAR ******** 
+
+let age = 30;
+age = 31;
+
+const birthYear = 1999;
+
+var job = "Programmer";
+job = "Teacher";
+
+deneme = 12;
+console.log(deneme);
+
+******** BASIC OPERATORS ******** */
+
+//Math Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageSarah / 2, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Smith";
+console.log(firstName + " " + lastName);
+
+// Assingment Operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 17);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
