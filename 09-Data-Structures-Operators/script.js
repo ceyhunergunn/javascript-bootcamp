f5232638c7865fa5934b94c82120ca7372be9772
ceyhunergunn <ceyhunergun61@gmail.com>
2024-02-01 22:27:42 +0300

    deneme

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -739,7 +739,7 @@ HINT 2: The solution only needs to work for a variable made out of 2 words, like
 HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
 HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
 
-Afterwards, test with your own test data!
+Afterwards, test with your own test data! 
 
 GOOD LUCK 😀
 
