49ba39a73143f7e56d759e8e92481c604938eb70
ceyhunergunn <ceyhunergun61@gmail.com>
2024-02-26 18:29:38 +0300

    18-forkify %40 completed.

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,13 +1,33 @@
-const recipeContainer = document.querySelector('.recipe');
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    // if (!id) return;
+    recipeView.renderSpinner(recipeContainer);
+
+    // 1) loading recipe
+    await model.loadRecipe(id);
+
+    // 2)rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    recipeView.renderError();
+  }
+};
+controlRecipes();
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
---
+++ 18-forkify/src/js/model.js
@@ -0,0 +1,25 @@
+import { API_URL } from './config';
+import { getJSON } from './helper';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/5ed6604591c37cdc054bc886`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
