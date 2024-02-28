61059fcb31404a8b1cccdcbe8ee095bbc054e32e
ceyhunergunn <ceyhunergun61@gmail.com>
2024-02-28 19:16:22 +0300

    18-forkify %80 completed.

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -2,32 +2,102 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import bookmarksView from './views/bookmarksView.js';
+import paginationView from './views/paginationView.js';
 
 ///////////////////////////////////////
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
 
-    // if (!id) return;
-    recipeView.renderSpinner(recipeContainer);
+    if (!id) return;
+    recipeView.renderSpinner();
 
-    // 1) loading recipe
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultPage());
+
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) loading recipe
     await model.loadRecipe(id);
 
-    // 2)rendering recipe
+    // 3)rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
     recipeView.renderError();
+    console.log(error);
   }
 };
-controlRecipes();
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1)Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2)Load search results
+    await model.loadSearchResults(query);
+
+    // 3)Render results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultPage());
+
+    // 4) render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render results
+  resultsView.render(model.getSearchResultPage(goToPage));
+
+  // 2) Render initial pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings(in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // 1) Add/Remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // 2) Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
 
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
--- 18-forkify/src/js/model.js
+++ 18-forkify/src/js/model.js
@@ -1,13 +1,17 @@
 import { API_URL } from './config';
 import { getJSON } from './helper';
+import { RES_PER_PAGE } from './config';
 
 export const state = {
   recipe: {},
+  search: { query: '', results: [], page: 1, resultsPerPage: RES_PER_PAGE },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/5ed6604591c37cdc054bc886`);
+    const data = await getJSON(`${API_URL}/${id}`);
+    // const data = await getJSON(`${API_URL}5ed6604591c37cdc054bc886`);
     const { recipe } = data.data;
     state.recipe = {
       id: recipe.id,
@@ -19,7 +23,86 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id)) {
+      state.recipe.bookmarked = true;
+    } else {
+      state.recipe.bookmarked = false;
+    }
   } catch (error) {
     throw error;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    state.search.page = 1;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getSearchResultPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    // ing.quantity = ing.quantity * (ing.servings / newServings);
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  });
+
+  state.recipe.servings = newServings;
+};
+
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  persistBookmarks();
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(bookmark => bookmark.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  persistBookmarks();
+};
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+
+// clearBookmarks();
